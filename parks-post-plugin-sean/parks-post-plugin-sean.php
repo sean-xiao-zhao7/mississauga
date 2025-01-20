3f9725171946bdc571623e93196bc11320c50a36
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-01-20 16:52:51 -0500

    Rename to ParksPostPluginSean

---
+++ parks-post-plugin-sean/parks-post-plugin-sean.php
@@ -0,0 +1,32 @@
+<?php
+
+/** 
+ * Name: ParksPostPluginSean
+ * Description: A plugin that registers the parks custom post type.
+ * Author: Sean Xiao
+ * Version: 1.0.0
+ * Text Domain: parks-post-plugin-sean
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class ParksPostPluginSean
+{
+    public function __construct()
+    {
+        add_action('init', [$this, 'create_post_type']);
+    }
+
+    public function create_post_type()
+    {
+        $args = [
+            'public' => true,
+            'has_archive' => true,
+            'supports' => ['title']
+        ];
+    }
+}
+
+new ParksPostPluginSean;
