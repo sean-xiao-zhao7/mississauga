49be2aeaa8bb0b84e8d8461f765adc8956373ae3
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-01-20 16:22:42 -0500

    Add ABSPATH check
    Exit if directly accessing plugin entry PHP.

--- posts-custom-plugin/posts-custom-plugin.php
+++ posts-custom-plugin/posts-custom-plugin.php
@@ -4,4 +4,10 @@
  * Name: Posts
  * Description: A simple WordPress plugin that registers a custom post type.
  * Author: Sean Xiao
+ * Version: 1.0.0
+ * Text Domain: posts-custom-plugin
  */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
