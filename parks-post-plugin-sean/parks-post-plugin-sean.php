c8689033c2a96c6b90fb3a3e2b734f7c421ee6ed
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-01-20 21:20:56 -0500

    Set add_meta_box on custom post type

--- parks-post-plugin-sean/parks-post-plugin-sean.php
+++ parks-post-plugin-sean/parks-post-plugin-sean.php
@@ -29,6 +29,7 @@ class Parks_Post_Plugin_Sean
     private function __construct()
     {
         add_action('init', [$this, 'ppps_create_post_type']);
+        // add_action('', [$this, 'add_custom_metabox']);
         add_action('wp_enqueue_scripts', [$this, 'ppps_load_assets']);
         add_shortcode('show-parks-list', [$this, 'ppps_show_parks_list']);
     }
@@ -46,12 +47,44 @@ class Parks_Post_Plugin_Sean
         return self::$instance;
     }
 
+    public function add_custom_metabox()
+    {
+        add_meta_box(
+            'custom_location_metabox',
+            'Location',
+            'render_custom_location_metabox',
+            'parks_post_sean',
+            'normal',
+            'default'
+        );
+    }
+
+    public function render_custom_location_metabox($post)
+    {
+        // Retrieve longitude and latitude values
+        $location = get_post_meta($post->ID, 'location', true);
+?>
+        <label for="location">Location:</label>
+        <input type="text" id="location" name="location" value="<?php echo esc_attr($location); ?>"><br>
+    <?php
+    }
+
+    function save_custom_location($post_id)
+    {
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+
+        // Save longitude and latitude values
+        if (isset($_POST['location'])) {
+            update_post_meta($post_id, 'longitude', sanitize_text_field($_POST['location']));
+        }
+    }
+
+
     public function ppps_create_post_type()
     {
         $args = [
             'public' => true,
             'has_archive' => true,
-            'supports' => ['name', 'location', 'hours', 'description'],
             'capability' => 'manage_options',
             'labels' => ['name' => 'Parks', 'singular_name' => 'Park'],
             'menu_icon' => 'dashicons-palmtree'
@@ -60,6 +93,7 @@ class Parks_Post_Plugin_Sean
         register_post_type('parks_post_sean', $args);
     }
 
+
     public function ppps_load_assets()
     {
         wp_enqueue_style('parks_post_sean', plugin_dir_url(__FILE__) . 'styles/main.css', [], 1, 'all');
@@ -67,7 +101,7 @@ class Parks_Post_Plugin_Sean
 
     public function ppps_show_parks_list()
     {
-?>
+    ?>
         <div id="parks-list-container">
             <h1>All Parks</h1>
             <table id="parks-list">
